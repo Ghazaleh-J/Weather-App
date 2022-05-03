3eb1ebf1b14ae977a1b9816e1411a456faad097a
Ghazaeh-J <ghazal.javadi@gmail.com>
2022-05-02 21:10:45 -0700

    Future Weather

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,18 @@
+// Variables
 var oneCallEndPointUrl = "https://api.openweathermap.org/data/2.5/onecall"
-var city = "Irvine"
+var city = ""
+var date = moment().format("MM/DD/YYYY")
+
 var apiKey = "fc9179d977bc1ac53ba567c6c820e30c"
 var weatherEndPointUrl = "https://api.openweathermap.org/data/2.5/weather"
 
+// DOM Element References
 var inputValue = document.getElementById("whatCity")
 var clickMe = document.getElementById("click")
 var currentWeatherContainer = document.getElementById("currentWeather")
+var futureWeatherContainer = document.getElementById("futureWeather")
+var eachDayContainer = document.getElementById("eachDay")
+
 
  function getWeather (){
 
@@ -24,6 +31,7 @@ var currentWeatherContainer = document.getElementById("currentWeather")
         .then (function (oneCallData){
             console.log(oneCallData)
             displayCurrentWeather(oneCallData);
+            displayFutureWeather(oneCallData);
         })
     })
 }
@@ -37,11 +45,53 @@ clickMe.addEventListener("click", function (event){
 
 function displayCurrentWeather (data){
 console.log(data.current)
-var humid = document.createElement("p");
-humid.innerText = "Humidity: " + data.current.humidity
-humid.setAttribute("class", "bg-success text-light")
+var cityDate = document.createElement("h3");
+var temp = document.createElement("p");
+var wind = document.createElement("p");
+var humidity = document.createElement("p");
+var uvIndex = document.createElement("p");
+var uvBadge = document.createElement("button");
+var tempKelvin = data.current.temp
+cityDate.innerText = `${city} (${date})`
+temp.innerText = "Temp: " + (((tempKelvin-273.15)*1.8)+32) + " °F"
+wind.innerText = "Wind: " + data.current.wind_speed + " MPH"
+humidity.innerText = "Humidity: " + data.current.humidity + " %"
+uvIndex.innerText = "UV Index: " 
+uvBadge.innerText =  data.current.uvi
+uvBadge.setAttribute("class", "btn")
+if (data.current.uvi < 3) {
+    uvBadge.setAttribute("class", "btn-success")
+} else if (data.current.uvi< 7) {
+    uvBadge.setAttribute("class", "btn-warning")
+} else {
+    uvBadge.setAttribute("class", "btn-danger")
+}
+
+uvIndex.append(uvBadge)
+
 var icons = document.createElement("img")
 icons.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon +"@2x.png" )
-currentWeatherContainer.append(humid, icons)
+currentWeatherContainer.append(cityDate, icons, temp, wind, humidity, uvIndex)
 
+}
+
+
+
+function displayFutureWeather (data){
+    console.log(data.daily) 
+    for (var i = 1; i < 6; i++) {
+        var tempp = document.createElement("p");
+        var windd = document.createElement("p");
+        var humidityy = document.createElement("p");
+        var temppKelvin = data.daily[i].temp.day
+        tempp.innerText = "Temp: " + (((temppKelvin-273.15)*1.8)+32) + " °F"  
+        windd.innerText = "Wind: " + data.daily[i].wind_speed + " MPH"
+        humidityy.innerText = "Humidity: " + data.daily[i].humidity + " %"
+        var iconss = document.createElement("img")
+        iconss.setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon +"@2x.png" )
+        // eachDayContainer.setAttribute("class", "col-md, card")
+        eachDayContainer.append(tempp, windd, humidityy, iconss)
+        futureWeatherContainer.append(eachDayContainer);
+
+    }
 }
